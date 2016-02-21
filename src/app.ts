d4b1c08dd2391b4064bd185be62a841d73d2296f
Balaji Chellappa <balaji.srgm@gmail.com>
2016-02-21 17:54:20 -0500

    Ver 1.1 Added main.js and moved bootstap fn call to it

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@
 import { Component, View, EventEmitter, provide, Input, Inject, enableProdMode } from 'angular2/core';
 import { CORE_DIRECTIVES } from 'angular2/common';
 import { bootstrap } from 'angular2/platform/browser';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, PathLocationStrategy, APP_BASE_HREF,
-LocationStrategy, HashLocationStrategy } from 'angular2/router';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, PathLocationStrategy, APP_BASE_HREF,LocationStrategy, HashLocationStrategy } from 'angular2/router';
+
 // Services
 import { MenuSvc } from './services/MenuSvc';
 import { DataSvc } from './services/DataSvc';
 import { SparkSvc } from './services/SparkSvc';
+
 // Wijmo Input components 
 import { wjNg2Input } from '../scripts/wijmo.angular2/wijmo.angular2.all';
 
@@ -110,13 +111,3 @@ export module explorer {
         }
     }
 }
-
-enableProdMode();
-// Bootstrap application with hash style navigation and global services.
-bootstrap(explorer.AppCmp, [
-    ROUTER_PROVIDERS,
-    provide(LocationStrategy, { useClass: HashLocationStrategy }),
-    MenuSvc,
-    DataSvc,
-    SparkSvc
-]);
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
-import { bootstrap } from "angular2/platform/browser";
-import { Component } from "angular2/core";
+// Angular
+import { Component, View, EventEmitter, provide, Input, Inject, enableProdMode } from 'angular2/core';
+import { CORE_DIRECTIVES } from 'angular2/common';
+import { bootstrap } from 'angular2/platform/browser';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, PathLocationStrategy, APP_BASE_HREF,LocationStrategy, HashLocationStrategy } from 'angular2/router';
 
-@Component({
-  selector: 'rx-data',
-  template: `
-  <div>
-    Hello world in Angular 2.0.1!!
-  </div>
-  `
-})
-class HelloWorld {
-}
+// Services
+import { MenuSvc } from './services/MenuSvc';
+import { DataSvc } from './services/DataSvc';
+import { SparkSvc } from './services/SparkSvc';
 
-bootstrap(HelloWorld);
+// App Component
+import { explorer } from './app';
+
+// uncommnet it to enable Production mode
+//enableProdMode();
+
+// Bootstrap application with hash style navigation and global services.
+bootstrap(explorer.AppCmp, [
+    ROUTER_PROVIDERS,
+    provide(LocationStrategy, { useClass: HashLocationStrategy }),
+    MenuSvc,
+    DataSvc,
+    SparkSvc
+]);
